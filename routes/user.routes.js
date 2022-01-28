c19d116353c2107c7218820e54b34c6c9420ed82
Victoire Nyoung <nyoungvictoire@gmail.com>
2022-01-28 13:04:43 +0100

    getAllUsers fait

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,11 @@
 const  router  = require("express").Router();
 const authController = require("../controllers/authController");
-
+const userController = require("../controllers/userController");
 
 // authentification
 router.post('/register', authController.signUp);
+
+router.get('/', userController.getAllUsers);
+
 
 module.exports = router;
