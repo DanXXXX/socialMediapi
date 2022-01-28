b7b355594fd5301f938a2c76fcde717dd0f149e1
Victoire Nyoung <nyoungvictoire@gmail.com>
2022-01-28 15:02:35 +0100

    get user:id fait

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,21 @@ const UserModel = require("../models/user.model");
 const ObjectId = require('mongoose').Types.ObjectId;
 
 module.exports.getAllUsers = async (req, res) => {
-    const users = await UserModel.find().select();
+    const users = await UserModel.find().select('-password');
     res.status(200).json(users);
+}
+
+module.exports.userInfo = (req, res) => {
+    console.log(req.params);
+    if (!ObjectId.isValid(req.params.id)) {
+        return res.status(400).send('ID unknom : ' + req.params.id)
+    }
+
+    UserModel.findById(req.params.id, (err, docs) => {
+        if (!err) {
+        res.send(docs); 
+        } else {
+        console.error('ID unknom : ' + err); 
+        }
+    }).select('-password');
 }
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -5,7 +5,10 @@ const userController = require("../controllers/userController");
 // authentification
 router.post('/register', authController.signUp);
 
+// voir tous les utilisateurs
 router.get('/', userController.getAllUsers);
+
+router.get('/:id', userController.userInfo)
 
 
 module.exports = router;
